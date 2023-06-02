72495d07fc5f1ed41ecb91046c0735c4708223b7
shekhar <shekhar21k@gmail.com>
2023-06-02 10:29:39 +0530

    final push

--- src/Components/stores/index.js
+++ src/Components/stores/index.js
@@ -1,13 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import registrationFormSlicereducer from './slices/registrationFormSlice';
 import tweetsSlicesreducer from "./slices/tweetsSlices";
-
+import thunkMiddleware from 'redux-thunk';
 
 const store = configureStore({
   reducer: {
     registration: registrationFormSlicereducer,
     tweets: tweetsSlicesreducer,
   },
+  middleware: [...getDefaultMiddleware(), thunkMiddleware],
 });
 
 export default store;
