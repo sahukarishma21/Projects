d25eb66cac47fce9e61fd1208bea8c6426abebda
Karishma Sahu <sahukarishma21@gmail.com>
2024-10-04 21:28:35 +0530

    Portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,38 +1,34 @@
 import React from "react";
 import "./About.css";
-import ME from '../../assets/Karishma.jpg';
+import ME from '../../assets/Karishma sahu.png';
+
 
 export default function About() {
   return (
     <section id="about">
       <hr />
-      
-      <h2>About Me</h2>
+      <div className="h33">
+      <h3>About Me</h3>
+      </div>
       <div className="container about__container">
         <div className="about__me">
-          <div className="about__me-image">
+        <div className="about__me-image">
           <img src={ME} alt="IMG" />
           </div>
         </div>
         <div className="about__content">
           <div className="about__card">
-            <h3> My Name Is Karishma Sahu, I am a Front-End Developer</h3>
+            <h3>Hello My Name Is Karishma Sahu</h3>
+            <h3>I am a Front-End Developer</h3>
             <br />
             <p>
-            As a passionate front-end developer,
-           I have gained proficiency in key technologies
-            such as JavaScript, ReactJS, HTML, CSS, and Redux.
-             Through my dedication and self-motivation, 
-             I continuously strive to enhance my skills and 
-             deliver exceptional website applications. My strong
-              work ethic ensures that I am committed to producing 
-              high-quality results.
+            I possess a strong skill set in JavaScript, Redux, React JS, HTML, CSS, MUI Icons, and Basic DSA. I have successfully completed various projects utilizing these skills. Additionally,
+             I actively participate in coding platforms such as FunctionUp to 
+             sharpen my abilities through challenges on platforms like LeetCode, Codezinger, and Codewars.
             </p>
             <br />
 
-            <a href="#contact" className="btn btn-primary">
-              Let's Talk
-            </a>
+            
           </div>
         </div>
       </div>
